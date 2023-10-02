6d560b35bd08f48bfa734646a6f602a61a06b55f
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-10-02 17:37:17 +0200

    Add files via upload

---
+++ dzielenie_integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int liczba_jablek, liczba_osob;
+ 
+ liczba_jablek = 15;
+ liczba_osob = 3;
+ 
+ cout << liczba_jablek / liczba_osob;
+
+ return 0;
+}
