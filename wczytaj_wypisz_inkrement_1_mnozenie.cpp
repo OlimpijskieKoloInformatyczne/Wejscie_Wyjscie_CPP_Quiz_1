113fefcadd3b3095e7839386cca9e870243a1a55
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-10-02 17:35:53 +0200

    Add files via upload

---
+++ wczytaj_wypisz_inkrement_1_mnozenie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int liczba_gesi, liczba_nog;
+ 
+ cin >> liczba_gesi;
+ ++liczba_gesi;
+ 
+ liczba_nog = 2 * liczba_gesi;
+ 
+ cout << liczba_nog;
+
+ return 0;
+}
